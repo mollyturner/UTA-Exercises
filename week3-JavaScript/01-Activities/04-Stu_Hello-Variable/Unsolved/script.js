e6d27b1695f779ad2b14724355a7a120fd5806dd
mollyturner <mollyturner50@gmail.com>
2023-01-31 00:11:12 -0600

    Add git ignore file

--- week3-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ week3-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,31 @@
 // Write Your JavaScript Code Here
+var personName = "Molly";
+var pets = 1;
+var funFact = "I like to travel";
+
+var myNameIs = "My name is ";
+var iHave = "I have ";
+var pet = " pet"
+var funFacts = "Fun Fact ";
+
+console.log (myNameIs + personName)
+console.log (iHave + pets + pet)
+console.log (funFacts + funFact)
+
+var personName = "Matt";
+var pets = 3;
+var funFact = "I like to eat";
+
+console.log (myNameIs + personName)
+console.log (iHave + pets + pet)
+console.log (funFacts + funFact)
+
+
+var a = 100;
+var b = 10;
+var c = "10";
+
+console.log (b == c) 
+console.log (b!= c)
+
+console.log (b === c)
--- week3-JavaScript/01-Activities/18-Stu_Scope/Unsolved/script.js
+++ week3-JavaScript/01-Activities/18-Stu_Scope/Unsolved/script.js
@@ -1,4 +1,6 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+var shout = "Shout";
+
 function justShout() {
   console.log(shout + ", " + shout);
   return;
@@ -13,6 +15,7 @@ justShout();
 shoutItAllOut();
 
 // TODO: Declare variable 'animal' with the value 'Tigers' so it is only available to the 'sayTigers' function
+
 function sayLions() {
   var animal = "Lions"; 
   console.log(animal);
@@ -20,6 +23,7 @@ function sayLions() {
 }
 
 function sayTigers() {
+  var animal = "Tigers"
   console.log("and " + animal + " and "); 
   return;
 }
@@ -28,7 +32,7 @@ function sayTigers() {
 var bears = "Bears";
 
 function sayBears() {
-    var bears = "Pandas";
+    //var bears = "Pandas";
     console.log(bears + "! OH MY!");
     return;
 }
@@ -38,9 +42,10 @@ sayTigers();
 sayBears();
 
 // TODO: The variable 'sing' should be declared once in the local scope.
-var sing = "Sing"; 
+
 
 function singAlong() {
+  var sing = "Sing"; 
   console.log(sing + ",");
   var singASong = function () {
     console.log(sing + " a Song.");
