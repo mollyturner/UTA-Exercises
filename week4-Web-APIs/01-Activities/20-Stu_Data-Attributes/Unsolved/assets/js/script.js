871da59079c6d7ada3542ccf3b2614d715158d6b
mollyturner <mollyturner50@gmail.com>
2023-01-31 20:13:14 -0600

    Activity complete

--- week4-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ week4-Web-APIs/01-Activities/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -2,6 +2,19 @@ var container = document.querySelector(".container");
 
 container.addEventListener("click", function(event) {
   var element = event.target;
+  var state = element.getAttribute('data-state');
+
+  console.log(state, "state");
+  // element.setAttribute("data-state", "data-number");
 
   // TODO: Complete function
+  if(state === 'hidden') {
+    var number = element.getAttribute('data-number');
+    element.textContent = number;
+    element.setAttribute('data-state', 'visible');
+  }
+  else {
+    element.textContent = '';
+    element.setAttribute('data-state', 'hidden');
+  }
 });
