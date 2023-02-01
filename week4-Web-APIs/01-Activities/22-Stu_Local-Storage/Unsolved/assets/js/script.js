5417f1d2e4ed9df4920203ed0ee0943f91fc4328
mollyturner <mollyturner50@gmail.com>
2023-02-01 12:08:37 -0600

    Activity complete

--- week4-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ week4-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -5,15 +5,22 @@ var msgDiv = document.querySelector("#msg");
 var userEmailSpan = document.querySelector("#user-email");
 var userPasswordSpan = document.querySelector("#user-password");
 
+function renderLastRegistered() {
+  // TODO: Retrieve the last email and password and render it to the page
+  var email = window.localStorage.getItem('email');
+  var password = window.localStorage.getItem('password');
+
+  if(email && password) {
+    userEmailSpan.textContent = email;
+    userPasswordSpan.textContent = password;
+  }
+}
+
 renderLastRegistered();
 
 function displayMessage(type, message) {
   msgDiv.textContent = message;
   msgDiv.setAttribute("class", type);
-}
-
-function renderLastRegistered() {
-  // TODO: Retrieve the last email and password and render it to the page
 }
 
 signUpButton.addEventListener("click", function(event) {
@@ -30,5 +37,7 @@ signUpButton.addEventListener("click", function(event) {
     displayMessage("success", "Registered successfully");
 
   // TODO: Save email and password to localStorage and render the last registered user
-  }
+    window.localStorage.setItem('email', email);
+    window.localStorage.setItem('password', password);
+}
 });
--- week4-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ week4-Web-APIs/01-Activities/24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -4,11 +4,19 @@ var emailInput = document.querySelector("#email");
 var passwordInput = document.querySelector("#password");
 var signUpButton = document.querySelector("#sign-up");
 
-signUpButton.addEventListener("click", function(event) {
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
   
-  // TODO: Create user object from submissio
+  // TODO: Create user object from submission
+  var userInfo = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
 
+  };
+  window.localStorage.setItem('userInfo', JSON.stringify(userInfo));
+  console.log(userInfo);
   // TODO: Set new submission to local storage 
   
 });
