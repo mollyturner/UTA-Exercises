b72083ecfc3c4ce21a0ec0ee9fa355ec87de2e64
mollyturner <mollyturner50@gmail.com>
2023-02-03 01:46:38 -0600

    Activity complete

--- week5-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ week5-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,21 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
+var input = $('input[name="#shopping-input"]');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+
+shoppingFormEl.on('submit', function (event) {
+    event.preventDefault();
+
+    var inputVal = input.val();
+    var listItem = $('<li>');
+    
+    listItem.text(inputVal);
+
+    shoppingListEl.append(listItem);
+
+    input.val('');
+});
+
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
--- week5-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ week5-Third-Party-APIs/01-Activities/08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -3,4 +3,8 @@ var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
 
+rootEl.children('ul').children('li').css('background-color', 'white');
+
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+
+rootEl.children('ul').eq(2).children().eq(0).text('O');
