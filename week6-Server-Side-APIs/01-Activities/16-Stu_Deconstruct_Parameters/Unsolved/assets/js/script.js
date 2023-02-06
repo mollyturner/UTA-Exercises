dfa7906aabcccf095c2c7f177c9ab62ef07fd889
mollyturner <mollyturner50@gmail.com>
2023-02-05 23:55:44 -0600

    Add completed activity

--- week6-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ week6-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -9,3 +9,7 @@ fetch(
     console.log(data);
   });
 // Parameter explanation.
+  // 1. First parameter is: per_page=10 - Will return a response of up to 10 results per page.
+  // 2. Second parameter is: state=open - Will only display reponses that are in the "open" state.
+  // 3. Third parameter is: sort=created - Specifies the sequence to display response. This will display by date created.
+  // 4. Fourth parameter is: direction=desc - Specifies the direction to display response and this will be in descending order.
