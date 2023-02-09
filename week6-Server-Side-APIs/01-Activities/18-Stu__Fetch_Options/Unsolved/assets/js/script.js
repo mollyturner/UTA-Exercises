ea4af6b902f8effe64b3da7cfe0a3239dac6425a
mollyturner <mollyturner50@gmail.com>
2023-02-08 21:53:55 -0600

    Add completed activity

--- week6-Server-Side-APIs/01-Activities/18-Stu__Fetch_Options/Unsolved/assets/js/script.js
+++ week6-Server-Side-APIs/01-Activities/18-Stu__Fetch_Options/Unsolved/assets/js/script.js
@@ -1,4 +1,10 @@
-fetch('https://api.github.com/repos/nodejs/node/issues?per_page=5')
+var fetchOptions = {
+  cache: 'reload',
+}
+
+fetch('https://api.github.com/repos/nodejs/node/issues?per_page=5', 
+fetchOptions
+)
   .then(function (response) {
     return response.json();
   })
--- week6-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ week6-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -3,5 +3,12 @@ var redirectUrl = './404.html';
 
 fetch(badRequestUrl).then(function (response) {
   // Use a conditional to check the response status.
+  if (response.status !== 200) {
+    document.location.replace(redirectUrl);
+  }
   // If that status equals the conditional, then redirect to the 404 page.
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
 });
