b0e6099c26d3295d8e4fba3bca7029a783897c9a
mollyturner <mollyturner50@gmail.com>
2023-02-09 15:20:21 -0600

    Add completed activity

---
+++ week9-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -0,0 +1,13 @@
+// music should be an object with title, artist, and album properties
+const music = {
+  // code here
+ title: 'Love Again',
+ artist: 'The Kid LAROI',
+ album: 'The First Time',
+};
+
+// Write code between the backticks tags to output the data from the music object above.
+const songSnippet = `The song ${music.title} by ${music.artist} on the album ${music.album} has been stuck in my head!`;
+
+console.log(songSnippet)
+
